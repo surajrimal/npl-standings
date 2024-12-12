bee3a36f01a9c8688fcd4f550820cd28974d8ff1
surajrimal <surajrimal50@gmail.com>
2024-12-12 12:38:28 -0500

    requirement updated

--- update_standings.py
+++ update_standings.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from app import create_app, db
 from app.models import Standing
 import unicodedata
-from selenium import webdriver
 import os
 import time
 
@@ -56,31 +55,6 @@ def extract_points_table():
 
         db.session.commit()
         print("Database updated successfully!")
-
-    generate_image(points_table)
-
-def generate_image(points_table):
-    IMG_FOLDER = 'app/static/assets/img'
-    os.makedirs(IMG_FOLDER, exist_ok=True)
-    
-    # Set up headless browser
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    #options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(options=options)
-
-    # Render the table as a web page
-    driver.get("https://nplstandings.com")
-    driver.set_window_size(1000, 900)
-    time.sleep(2)  # Allow time for loading
-
-    # Capture screenshot
-    screenshot_path = os.path.join(IMG_FOLDER, 'standings.png')
-    driver.save_screenshot(screenshot_path)
-    driver.quit()
-
-    print(f"Generated image at: {screenshot_path}")
 
 if __name__ == "__main__":
     extract_points_table()
